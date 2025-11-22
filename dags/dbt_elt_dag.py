e65e8445be6e7c16f8632e540d3ae7ee3b3fbaef
Benita <benita.isaac@gmail.com>
2025-11-22 00:25:28 -0800

    added dbt snapshot

--- dags/dbt_elt_dag.py
+++ dags/dbt_elt_dag.py
@@ -4,34 +4,35 @@ from datetime import datetime, timedelta
 
 default_args = {"owner": "you", "retries": 1, "retry_delay": timedelta(minutes=5)}
 
-DBT_DIR = "/opt/airflow/dbt/stock_analytics"
-DBT = "/home/airflow/.local/bin/dbt"  
+DBT_PROJECT_DIR = "/opt/airflow/dbt/stock_analytics"
+DBT_PROFILES_DIR = "/opt/airflow/dbt"
+DBT = "/home/airflow/dbtvenv/bin/dbt"  # use the venv dbt binary directly
 
 with DAG(
     dag_id="dbt_elt_pipeline",
     default_args=default_args,
     start_date=datetime(2025, 1, 1),
-    schedule_interval=None,
+    schedule=None,              # triggered by your ETL DAG
     catchup=False,
     tags=["dbt", "elt"],
 ) as dag:
 
-    run_dbt = BashOperator(
-    task_id="run_dbt_models",
-    bash_command=(
-        "source /home/airflow/dbtvenv/bin/activate && "
-        "cd /opt/airflow/dbt/stock_analytics && "
-        "dbt run --profiles-dir /opt/airflow/dbt"
-    ),
-)
-
-    test_dbt = BashOperator(
-        task_id="test_dbt_models",
-        bash_command=(
-            "source /home/airflow/dbtvenv/bin/activate && "
-            "cd /opt/airflow/dbt/stock_analytics && "
-            "dbt test --profiles-dir /opt/airflow/dbt"
-        ),
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        env={"DBT_PROFILES_DIR": DBT_PROFILES_DIR},
+        bash_command=f"cd {DBT_PROJECT_DIR} && {DBT} run"
     )
 
-    run_dbt >> test_dbt
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        env={"DBT_PROFILES_DIR": DBT_PROFILES_DIR},
+        bash_command=f"cd {DBT_PROJECT_DIR} && {DBT} test"
+    )
+
+    dbt_snapshot = BashOperator(
+        task_id="dbt_snapshot",
+        env={"DBT_PROFILES_DIR": DBT_PROFILES_DIR},
+        bash_command=f"cd {DBT_PROJECT_DIR} && {DBT} snapshot"
+    )
+
+    dbt_run >> dbt_test >> dbt_snapshot
